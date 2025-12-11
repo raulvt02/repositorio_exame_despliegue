6679ebffc80278c2c3bbee336d165cc516491ec8
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-12-11 12:46:13 +0100

    subido

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+
+define('BD_SERVIDOR','mysql-rauulvt.alwaysdata.net');
+define('BD_USUARIO','rauulvt');
+define('BD_SENHA','3.examen.3');
+define('BD_BANCO','rauulvt_database');
+
